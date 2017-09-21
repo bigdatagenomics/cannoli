1e4eb0d62fc0167a977d152201f8898790e78510
Gunjan Baid <gunjanbaid@users.noreply.github.com>
2017-09-21 14:15:51 -0700

    Update run-macs2.sh

--- run-macs2.sh
+++ run-macs2.sh
@@ -1,17 +1,20 @@
+
+
 #!/bin/bash
 
 set +x
 
-# save full filepath that is piped in
-filePath=`cat`
-# save just the filename
-fileName=`basename ${filePath}`
-# save location where all MACS2 output will be saved
-outputPath=$1
+tee datafile > /dev/null
+rm -r -f NA_summits.bed
+/home/eecs/gunjan/envs/gunjan/bin/macs2 callpeak -t datafile --verbose 0
+#fileName=HELLO
+#awk -v file="$fileName" 'BEGIN{OFS="\t"}{$4=file}1' NA_summits.bed
+cat NA_summits.bed
 
-# run macs2 callpeak function
-# -t is input file, -n is prefix for output filenames, --outdir is location where files saved
-macs2 callpeak -t ${filePath} -n ${fileName} --outdir ${outputPath}/${fileName}_output
+#tee hi.txt > /dev/null
+#cp /home/eecs/gunjan/all-adam/cannoli/test.bed gunjan.bed
+#cat gunjan.bed
+#/home/eecs/gunjan/envs/gunjan/bin/macs2 callpeak -t test.bed -n HI
 
-# pipe out BED file entries from MACS2 output file
-echo ${outputPath}/${fileName}_output/${fileName}_summits.bed
+#cat /home/eecs/gunjan/all-adam/cannoli/test.bed
+#cat HI_summits.bed
