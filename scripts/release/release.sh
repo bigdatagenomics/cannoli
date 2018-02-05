50b49213424fde146d9673318cca76a39f49153c
Michael L Heuer <heuermh@acm.org>
2018-02-05 14:28:40 -0600

    Dropping support for Scala 2.10 and Spark 1.x.

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -24,64 +24,6 @@ echo "releasing from ${commit} on branch ${branch}"
 
 git push origin ${branch}
 
-# do scala 2.10 release
-git checkout -b maint_2.10-${release} ${branch}
-mvn --batch-mode \
-  -P distribution \
-  -Dresume=false \
-  -Dtag=cannoli-parent_2.10-${release} \
-  -DreleaseVersion=${release} \
-  -DdevelopmentVersion=${devel} \
-  -DbranchName=cannoli_2.10-${release} \
-  release:clean \
-  release:prepare \
-  release:perform
-
-if [ $? != 0 ]; then
-  echo "Releasing Spark 1, Scala 2.10 version failed."
-  exit 1
-fi
-
-# do scala 2.11 release
-git checkout -b maint_2.11-${release} ${branch}
-./scripts/move_to_scala_2.11.sh
-git commit -a -m "Modifying pom.xml files for Spark 1, Scala 2.11 release."
-mvn --batch-mode \
-  -P distribution \
-  -Dresume=false \
-  -Dtag=cannoli-parent_2.11-${release} \
-  -DreleaseVersion=${release} \
-  -DdevelopmentVersion=${devel} \
-  -DbranchName=cannoli_2.11-${release} \
-  release:clean \
-  release:prepare \
-  release:perform
-
-if [ $? != 0 ]; then
-  echo "Releasing Spark 1, Scala 2.11 version failed."
-  exit 1
-fi
-
-# do spark 2, scala 2.10 release
-git checkout -b maint_spark2_2.10-${release} ${branch}
-./scripts/move_to_spark_2.sh
-git commit -a -m "Modifying pom.xml files for Spark 2, Scala 2.10 release."
-mvn --batch-mode \
-  -P distribution \
-  -Dresume=false \
-  -Dtag=cannoli-parent-spark2_2.10-${release} \
-  -DreleaseVersion=${release} \
-  -DdevelopmentVersion=${devel} \
-  -DbranchName=cannoli-spark2_2.10-${release} \
-  release:clean \
-  release:prepare \
-  release:perform
-
-if [ $? != 0 ]; then
-  echo "Releasing Spark 2, Scala 2.10 version failed."
-  exit 1
-fi
-
 # do spark 2, scala 2.11 release
 git checkout -b maint_spark2_2.11-${release} ${branch}
 ./scripts/move_to_spark_2.sh
