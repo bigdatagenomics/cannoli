4afdc5910fd46bf584d8f78c1c576bd70cd9a7cf
Michael L Heuer <heuermh@acm.org>
2019-05-06 11:48:28 -0500

    Update minimap2 default docker image to version 2.17.

--- core/src/main/scala/org/bdgenomics/cannoli/Minimap2.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/Minimap2.scala
@@ -36,8 +36,8 @@ class Minimap2Args extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the Minimap2 executable. Defaults to minimap2.")
   var executable: String = "minimap2"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/minimap2:2.16--h84994c4_1.")
-  var image: String = "quay.io/biocontainers/minimap2:2.16--h84994c4_1"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/minimap2:2.17--h84994c4_0.")
+  var image: String = "quay.io/biocontainers/minimap2:2.17--h84994c4_0"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
