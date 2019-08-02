290c669addef9954e07bff76cce6826ce0575306
Michael L Heuer <heuermh@acm.org>
2019-08-02 11:01:43 -0500

    Bump Docker image for freebayes.

--- core/src/main/scala/org/bdgenomics/cannoli/Freebayes.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/Freebayes.scala
@@ -39,8 +39,8 @@ class FreebayesArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the Freebayes executable. Defaults to freebayes.")
   var executable: String = "freebayes"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/freebayes:1.2.0--py27h56106d0_4.")
-  var image: String = "quay.io/biocontainers/freebayes:1.2.0--py27h56106d0_4"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/freebayes:1.3.1--py37h56106d0_0.")
+  var image: String = "quay.io/biocontainers/freebayes:1.3.1--py37h56106d0_0"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
