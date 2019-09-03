b598f770b7777912b77c08f4fe2adde0d4efb273
Michael L Heuer <heuermh@acm.org>
2019-09-03 09:48:59 -0500

    Bump Docker image for bcftools.

--- core/src/main/scala/org/bdgenomics/cannoli/BcftoolsMpileup.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/BcftoolsMpileup.scala
@@ -36,8 +36,8 @@ class BcftoolsMpileupArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the bcftools executable. Defaults to bcftools.")
   var executable: String = "bcftools"
 
-  @Args4jOption(required = false, name = "-docker_image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.9--ha228f0b_3.")
-  var image: String = "quay.io/biocontainers/bcftools:1.9--ha228f0b_3"
+  @Args4jOption(required = false, name = "-docker_image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.9--h5c2b69b_6.")
+  var image: String = "quay.io/biocontainers/bcftools:1.9--h5c2b69b_6"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
