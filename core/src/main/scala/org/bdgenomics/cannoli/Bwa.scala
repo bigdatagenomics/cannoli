2b9a23c420924b9e8b10787eacd0d209ab3f3fc4
Michael L Heuer <heuermh@acm.org>
2020-01-14 09:55:23 -0600

    Update container image versions.

--- core/src/main/scala/org/bdgenomics/cannoli/Bwa.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/Bwa.scala
@@ -39,8 +39,8 @@ class BwaArgs extends ReadGroupArgs {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the BWA executable. Defaults to bwa.")
   var executable: String = "bwa"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bwa:0.7.17--hed695b0_6.")
-  var image: String = "quay.io/biocontainers/bwa:0.7.17--hed695b0_6"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bwa:0.7.17--hed695b0_7.")
+  var image: String = "quay.io/biocontainers/bwa:0.7.17--hed695b0_7"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
