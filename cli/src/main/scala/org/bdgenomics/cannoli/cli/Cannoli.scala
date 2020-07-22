63e4b2a141c972672d2a3e0f3247f6e79f42b4ea
Michael L Heuer <heuermh@acm.org>
2020-07-22 14:50:10 -0500

    Add bwa-mem2 for aligning fragments.

--- cli/src/main/scala/org/bdgenomics/cannoli/cli/Cannoli.scala
+++ cli/src/main/scala/org/bdgenomics/cannoli/cli/Cannoli.scala
@@ -37,6 +37,7 @@ object Cannoli {
     Bowtie2,
     SingleEndBowtie2,
     Bwa,
+    BwaMem2,
     Freebayes,
     Gem,
     MagicBlast,
---
+++ core/src/main/scala/org/bdgenomics/cannoli/BwaMem2.scala
@@ -0,0 +1,146 @@
+/**
+ * Licensed to Big Data Genomics (BDG) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The BDG licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bdgenomics.cannoli
+
+import org.apache.hadoop.fs.{ FileSystem, Path }
+import org.apache.spark.SparkContext
+import org.bdgenomics.adam.rdd.ADAMContext._
+import org.bdgenomics.adam.rdd.fragment.{ FragmentDataset, InterleavedFASTQInFormatter }
+import org.bdgenomics.adam.models.ReadGroupDictionary
+import org.bdgenomics.adam.rdd.read.{ AlignmentDataset, AnySAMOutFormatter }
+import org.bdgenomics.adam.sql.{ Alignment => AlignmentProduct }
+import org.bdgenomics.cannoli.builder.CommandBuilders
+import org.bdgenomics.formats.avro.Alignment
+import org.kohsuke.args4j.{ Option => Args4jOption }
+import scala.collection.JavaConversions._
+
+/**
+ * BwaMem2 function arguments.
+ */
+class BwaMem2Args extends ReadGroupArgs {
+  @Args4jOption(required = true, name = "-index", usage = "Path to the Bwa-mem2 index to be searched, e.g. <idxbase> in bwa-mem2 [options]* <idxbase>. Required.")
+  var indexPath: String = null
+
+  @Args4jOption(required = false, name = "-executable", usage = "Path to the Bwa-mem2 executable. Defaults to bwa-mem2.")
+  var executable: String = "bwa-mem2"
+
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bwa-mem2:2.0--he513fc3_0.")
+  var image: String = "quay.io/biocontainers/bwa-mem2:2.0--he513fc3_0"
+
+  @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
+  var sudo: Boolean = false
+
+  @Args4jOption(required = false, name = "-add_files", usage = "If true, use the SparkFiles mechanism to distribute files to executors.")
+  var addFiles: Boolean = false
+
+  @Args4jOption(required = false, name = "-use_docker", usage = "If true, uses Docker to launch Bwa-mem2.")
+  var useDocker: Boolean = false
+
+  @Args4jOption(required = false, name = "-use_singularity", usage = "If true, uses Singularity to launch Bwa-mem2.")
+  var useSingularity: Boolean = false
+}
+
+/**
+ * BwaMem2 wrapper as a function FragmentDataset &rarr; AlignmentDataset,
+ * for use in cannoli-shell or notebooks.
+ *
+ * @param args BwaMem2 function arguments.
+ * @param sc Spark context.
+ */
+class BwaMem2(
+    val args: BwaMem2Args,
+    sc: SparkContext) extends CannoliFn[FragmentDataset, AlignmentDataset](sc) {
+
+  override def apply(fragments: FragmentDataset): AlignmentDataset = {
+
+    def getIndexPaths(fastaPath: String): Seq[String] = {
+      val requiredExtensions = Seq("",
+        ".amb",
+        ".ann",
+        ".bwt.2bit.64",
+        ".bwt.8bit.32",
+        ".pac")
+      val optionalExtensions = Seq(".alt", ".sa", ".0123")
+
+      // oddly, the hadoop fs apis don't seem to have a way to do this?
+      def canonicalizePath(fs: FileSystem, path: Path): String = {
+        val fsUri = fs.getUri()
+        new Path(fsUri.getScheme, fsUri.getAuthority,
+          Path.getPathWithoutSchemeAndAuthority(path).toString).toString
+      }
+
+      def optionalPath(ext: String): Option[String] = {
+        val path = new Path(fastaPath + ext)
+        val fs = path.getFileSystem(sc.hadoopConfiguration)
+        if (fs.exists(path)) {
+          Some(canonicalizePath(fs, path))
+        } else {
+          None
+        }
+      }
+
+      val pathsWithScheme = requiredExtensions.map(ext => {
+        optionalPath(ext).getOrElse({
+          throw new IllegalStateException(
+            "Required index file %s%s does not exist.".format(fastaPath, ext))
+        })
+      })
+
+      val optionalPathsWithScheme = optionalExtensions.flatMap(optionalPath)
+
+      pathsWithScheme ++ optionalPathsWithScheme
+    }
+
+    val readGroup = Option(args.readGroup).getOrElse(args.createReadGroup)
+
+    val builder = CommandBuilders.create(args.useDocker, args.useSingularity)
+      .setExecutable(args.executable)
+      .add("mem")
+      .add("-t")
+      .add("1")
+      .add("-R")
+      .add(readGroup.toSAMReadGroupRecord().getSAMString().replace("\t", "\\t"))
+      .add("-p")
+      .add(if (args.addFiles) "$0" else args.indexPath)
+      .add("-")
+
+    if (args.addFiles) {
+      getIndexPaths(args.indexPath).foreach(builder.addFile(_))
+    }
+
+    if (args.useDocker || args.useSingularity) {
+      builder
+        .setImage(args.image)
+        .setSudo(args.sudo)
+        .addMount(if (args.addFiles) "$root" else root(args.indexPath))
+    }
+
+    info("Piping %s to bwa-mem2 with command: %s files: %s".format(
+      fragments, builder.build(), builder.getFiles()))
+
+    implicit val tFormatter = InterleavedFASTQInFormatter
+    implicit val uFormatter = new AnySAMOutFormatter
+
+    val alignments = fragments.pipe[Alignment, AlignmentProduct, AlignmentDataset, InterleavedFASTQInFormatter](
+      cmd = builder.build(),
+      files = builder.getFiles()
+    )
+
+    alignments.replaceReadGroups(ReadGroupDictionary(Seq(readGroup)))
+  }
+}
