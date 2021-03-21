92f693e81e232b3d62c59f8bd6f9a3bb0f61f41c
Michael L Heuer <heuermh@acm.org>
2021-03-21 16:28:11 -0500

    Update default container images for bcftools and samtools to 1.12.

--- core/src/main/scala/org/bdgenomics/cannoli/BcftoolsMpileup.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/BcftoolsMpileup.scala
@@ -36,8 +36,8 @@ class BcftoolsMpileupArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the bcftools executable. Defaults to bcftools.")
   var executable: String = "bcftools"
 
-  @Args4jOption(required = false, name = "-docker_image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.11--h7c999a4_0.")
-  var image: String = "quay.io/biocontainers/bcftools:1.11--h7c999a4_0"
+  @Args4jOption(required = false, name = "-docker_image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.12--h3f113a9_0.")
+  var image: String = "quay.io/biocontainers/bcftools:1.12--h3f113a9_0"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
