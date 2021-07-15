379387f3aa386ae831088c4d9af6496f21d012c4
Michael L Heuer <heuermh@acm.org>
2021-07-15 11:24:14 -0500

    Update bcftools container image to version 1.13--h3a49de5_0.

--- core/src/main/scala/org/bdgenomics/cannoli/BcftoolsCall.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/BcftoolsCall.scala
@@ -35,8 +35,8 @@ class BcftoolsCallArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the bcftools executable. Defaults to bcftools.")
   var executable: String = "bcftools"
 
-  @Args4jOption(required = false, name = "-docker_image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.12--h3f113a9_0.")
-  var image: String = "quay.io/biocontainers/bcftools:1.12--h3f113a9_0"
+  @Args4jOption(required = false, name = "-docker_image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.13--h3a49de5_0.")
+  var image: String = "quay.io/biocontainers/bcftools:1.13--h3a49de5_0"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
