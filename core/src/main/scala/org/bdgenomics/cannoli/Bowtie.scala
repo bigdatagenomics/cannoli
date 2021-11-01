6cfa7a9ac3aa3b3c315375c1623ee7002a935c20
Michael L Heuer <heuermh@acm.org>
2021-11-01 09:29:11 -0500

    Update bowtie container image to 1.3.1--py39h176da8b_0.

--- core/src/main/scala/org/bdgenomics/cannoli/Bowtie.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/Bowtie.scala
@@ -35,8 +35,8 @@ class BowtieArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the Bowtie executable. Defaults to bowtie.")
   var executable: String = "bowtie"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bowtie:1.3.0--py38hed8969a_1.")
-  var image: String = "quay.io/biocontainers/bowtie:1.3.0--py38hed8969a_1"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bowtie:1.3.1--py39h176da8b_0.")
+  var image: String = "quay.io/biocontainers/bowtie:1.3.1--py39h176da8b_0"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
