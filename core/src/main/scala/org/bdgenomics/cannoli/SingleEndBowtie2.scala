7408a8048d7be074cc4161501bdf2ae25edd1e27
Michael L Heuer <heuermh@acm.org>
2022-01-18 10:17:28 -0600

    Update bowtie2 container image to 2.4.4--py38he5f0661_1.

--- core/src/main/scala/org/bdgenomics/cannoli/SingleEndBowtie2.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/SingleEndBowtie2.scala
@@ -39,8 +39,8 @@ class SingleEndBowtie2Args extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the Bowtie 2 executable. Defaults to bowtie2.")
   var executable: String = "bowtie2"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bowtie2:2.4.4--py38h72fc82f_0.")
-  var image: String = "quay.io/biocontainers/bowtie2:2.4.4--py38h72fc82f_0"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bowtie2:2.4.4--py38he5f0661_1.")
+  var image: String = "quay.io/biocontainers/bowtie2:2.4.4--py38he5f0661_1"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
