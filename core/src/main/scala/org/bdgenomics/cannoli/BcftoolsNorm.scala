ae8304e7698054eb75f4154b5e75548a6ab43191
Michael L Heuer <heuermh@acm.org>
2022-10-19 11:34:31 -0500

    Update bcftools container image to 1.16--hfe4b78e_1.

--- core/src/main/scala/org/bdgenomics/cannoli/BcftoolsNorm.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/BcftoolsNorm.scala
@@ -39,8 +39,8 @@ class BcftoolsNormArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the bcftools executable. Defaults to bcftools.")
   var executable: String = "bcftools"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.15.1--h0ea216a_0.")
-  var image: String = "quay.io/biocontainers/bcftools:1.15.1--h0ea216a_0"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bcftools:1.16--hfe4b78e_1.")
+  var image: String = "quay.io/biocontainers/bcftools:1.16--hfe4b78e_1"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
