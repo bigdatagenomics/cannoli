b80ad023ab2cf6246d7d34742e475910721b86cd
Michael L Heuer <heuermh@acm.org>
2023-04-28 10:44:18 -0500

    Update blast container image to 2.13.0--hf3cf87c_1.

--- core/src/main/scala/org/bdgenomics/cannoli/Blastn.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/Blastn.scala
@@ -37,8 +37,8 @@ class BlastnArgs extends Args4jBase {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the blastn executable. Defaults to blastn.")
   var executable: String = "blastn"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/blast:2.13.0--hf3cf87c_0.")
-  var image: String = "quay.io/biocontainers/blast:2.13.0--hf3cf87c_0"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/blast:2.13.0--hf3cf87c_1.")
+  var image: String = "quay.io/biocontainers/blast:2.13.0--hf3cf87c_1"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
