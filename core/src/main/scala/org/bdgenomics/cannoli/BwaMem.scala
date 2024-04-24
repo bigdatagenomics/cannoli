621c682a6710e9f92280e1786bacd9b0b15a231e
Michael L Heuer <heuermh@acm.org>
2024-04-24 14:08:31 -0500

    Update bwa container image to 0.7.18--he4a0461_0.

--- core/src/main/scala/org/bdgenomics/cannoli/BwaMem.scala
+++ core/src/main/scala/org/bdgenomics/cannoli/BwaMem.scala
@@ -38,8 +38,8 @@ class BwaMemArgs extends ReadGroupArgs {
   @Args4jOption(required = false, name = "-executable", usage = "Path to the BWA executable. Defaults to bwa.")
   var executable: String = "bwa"
 
-  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bwa:0.7.17--h7132678_9.")
-  var image: String = "quay.io/biocontainers/bwa:0.7.17--h7132678_9"
+  @Args4jOption(required = false, name = "-image", usage = "Container image to use. Defaults to quay.io/biocontainers/bwa:0.7.18--he4a0461_0.")
+  var image: String = "quay.io/biocontainers/bwa:0.7.18--he4a0461_0"
 
   @Args4jOption(required = false, name = "-sudo", usage = "Run via sudo.")
   var sudo: Boolean = false
